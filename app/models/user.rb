b7fa80a173ee7363ed0c7c714f8e4e473c7b58d9
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-05-03 03:03:14 +0000

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :tasks, dependent: :destroy
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :name, presence: true
+  validates :email_address, presence: true, uniqueness: { case_sensitive: false }
 end
