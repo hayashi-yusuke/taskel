b602ea12daee748f111490703c357a4387fe0b9a
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-05-06 03:51:45 +0000

    Add task index page and Bootstrap styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.where(completed: false)
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks.where(completed: false)
   end
 
   def edit
