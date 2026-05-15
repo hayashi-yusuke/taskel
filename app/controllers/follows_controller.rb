04af577a4159a74d88896628c73a1d2fe8788472
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-05-15 15:32:38 +0000

    Add follow feature (controller, view)

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,19 @@
 class FollowsController < ApplicationController
   def create
-    @follow = Current.user.follower.built(:)
+    @user = User.find(params[:user_id])
+    follow = Current.user.active_relationships.build(following: @user)
+    
+    if follow.save
+      redirect_back fallback_location: root_path, notice: "フォローしました！"
+    else
+      redirect_back fallback_location: root_path, alert: "フォローに失敗しました"
+    end
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    follow = Current.user.active_relationships.find_by(following: @user)
+    follow&.destroy
+    redirect_back fallback_location: root_path, notice: "フォローを解除しました"
+  end
 end
