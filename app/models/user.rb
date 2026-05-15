f146463bad47e2a04069df7fb27da60aac81bbb0
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-05-15 05:05:04 +0000

    Add follows routing and relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: :follower_id,
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: :following_id,
+                                   dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :following
+  has_many :followers, through: :passive_relationships, source: :follower
   
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   get "/about", to: "homes#about", as: :about
 
   get "/mypage", to: "users#mypage", as: :mypage
-  resources :users, only: [ :index, :new, :create, :edit, :show, :update, :destroy ]
+  resources :users, only: [ :index, :new, :create, :edit, :show, :update, :destroy ] do
+    resource :follow, only: [ :create, :destroy ]
+  end
 
   resources :tasks, only: [ :index, :show, :create, :edit, :update, :destroy ] do
     member do
