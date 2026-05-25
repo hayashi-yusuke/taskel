63cbbe09bd7f97cf965e858b916f338a6d1475c6
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-05-25 13:17:07 +0000

    Fix CI test failures: fixtures, controller tests, and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+  generates_token_for :password_reset, expires_in: 15.minutes do
+    password_salt&.last(10)
+  end
+
   has_many :sessions, dependent: :destroy
   has_many :tasks, dependent: :destroy
   has_many :comments, dependent: :destroy
--- test/test_helpers/session_test_helper.rb
+++ test/test_helpers/session_test_helper.rb
@@ -8,6 +8,15 @@ module SessionTestHelper
     end
   end
 
+  def sign_in_as_admin(admin)
+    Current.session = admin.sessions.create!
+
+    ActionDispatch::TestRequest.create.cookie_jar.tap do |cookie_jar|
+      cookie_jar.signed[:admin_session_id] = Current.session.id
+      cookies["admin_session_id"] = cookie_jar[:admin_session_id]
+    end
+  end
+
   def sign_out
     Current.session&.destroy!
     cookies.delete("session_id")
