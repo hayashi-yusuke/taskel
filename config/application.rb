bc191cfcf56b012b68d02c6c477d0fd2bb126128
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-06-05 14:39:48 +0000

    Fix RuboCop offenses

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Taskel
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
     config.i18n.default_locale = :ja
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.active_support.use_solid_cache = false
 
     # Configuration for the application, engines, and railties goes here.
